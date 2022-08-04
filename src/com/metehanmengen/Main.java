f0565b94a69639741df06889e2537461bdb5132a
[metehan mengen] <[valid-email]>
2022-08-04 14:35:16 +0300

    factorial method created

---
+++ src/com/metehanmengen/Main.java
@@ -0,0 +1,21 @@
+package com.metehanmengen;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner;
+
+        System.out.println("Bir sayı giriniz");
+        var a = Integer.parseInt(kb.nextLine());
+        int result = 1;
+
+        for (int i = 2; i < n; i++)
+        {
+            result *= i;
+        }
+
+        System.out.println(result);
+
+    }
+}
