691bed434d670a2e410f531c8800703076e0de3a
[metehan mengen] <[valid-email]>
2022-08-10 12:06:22 +0300

    Date

---
+++ Sample/src/com/metehanmengen/app/exercises/powFindingCalculator.java
@@ -0,0 +1,21 @@
+package com.metehanmengen.app.exercises;
+
+import java.util.Scanner;
+
+public class powFindingCalculator {
+    public static void calculator()
+    {
+        Scanner kb = new Scanner(System.in);
+        int n = Integer.parseInt(kb.nextLine());
+
+        for (int i = 1; i <= n; i*=3)
+        {
+            System.out.println(i);
+        }
+
+        for (int i = 1; i <= n; i*=4)
+        {
+            System.out.println(i);
+        }
+    }
+}
