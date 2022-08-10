a2d41547220131985fca2e2d945b1eb7f8d8cf70
[metehan mengen] <[valid-email]>
2022-08-10 17:23:37 +0300

    RecursivePow

---
+++ Sample/src/com/metehanmengen/app/exercises/RecursivePatternMethods.java
@@ -0,0 +1,42 @@
+package com.metehanmengen.app.exercises;
+
+import java.util.Scanner;
+
+public class RecursivePatternMethods {
+    private int m_count;
+    int setFiveMinus(int n)
+    {
+        System.out.print(n + " ");
+
+        if (n <= 0)
+            return setFivePlus(n);
+
+        return setFiveMinus(n - 5);
+    }
+
+    int setFivePlus(int n)
+    {
+        if(n == m_count)
+            return 1;
+        n += 5;
+        System.out.print(n + " ");
+
+        return setFivePlus(n);
+
+    }
+
+    public void setFiveMinus()
+    {
+
+        Scanner kb = new Scanner(System.in);
+        System.out.println("bir sayı giriniz :");
+        int n = Integer.parseInt(kb.nextLine());
+        m_count = n;
+
+        setFiveMinus(n);
+        System.out.println();
+
+
+
+    }
+}
