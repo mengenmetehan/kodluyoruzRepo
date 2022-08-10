a1a2318f7236cfc1d1286a04f886c547956eb9e0
[metehan mengen] <[valid-email]>
2022-08-10 09:21:21 +0300

    taximeter Calculator

---
+++ Sample/src/app/app/exercises/BMI_Index.java
@@ -0,0 +1,18 @@
+package app.app.exercises;
+
+import java.util.Scanner;
+
+public class BMI_Index {
+    public static void calculator()
+    {
+        System.out.println("Kilonuzu giriniz :");
+        Scanner kb = new Scanner(System.in);
+
+        int weight = Integer.parseInt(kb.nextLine());
+        System.out.println("Boyunuzu giriniz :");
+        int height = Integer.parseInt(kb.nextLine());
+
+        System.out.printf("BMI : %02f", (double)weight / height * height );
+
+    }
+}
