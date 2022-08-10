278b155e6d952d3a8011f5019c55136454d6cbd7
[metehan mengen] <[valid-email]>
2022-08-10 12:26:39 +0300

    Factorial

---
+++ Sample/src/com/metehanmengen/app/exercises/Harmonic.java
@@ -0,0 +1,19 @@
+package com.metehanmengen.app.exercises;
+
+import java.util.Scanner;
+
+public class Harmonic {
+    public static void calculator()
+    {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz:");
+        int n = Integer.parseInt(kb.nextLine());
+        double count = 1.0;
+
+        for (int i = 2; i <= n; i++)
+            count += 1. / i;
+
+        System.out.println("total harmonik sayı " + count);
+
+    }
+}
