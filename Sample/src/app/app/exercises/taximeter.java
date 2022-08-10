2caf9ad21662d361ad45175ecb45d7956d6eba62
[metehan mengen] <[valid-email]>
2022-08-10 09:08:57 +0300

    taximeter Calculator

---
+++ Sample/src/app/app/exercises/taximeter.java
@@ -0,0 +1,18 @@
+package app.app.exercises;
+
+import java.util.Scanner;
+
+public class taximeter {
+    public static void main(String[] args) {
+        double km;
+        int initiatePrice= 10 ;
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Km Bilgisi: ");
+        km = kb.nextDouble();
+        double price = (km * 2.20) + initiatePrice;
+        boolean minTutar;
+        minTutar = (price <= 20)&(price > 0);
+        price  = minTutar ? 20 : price ;
+        System.out.println("Taksı Ucretı: "+ price );
+    }
+}
