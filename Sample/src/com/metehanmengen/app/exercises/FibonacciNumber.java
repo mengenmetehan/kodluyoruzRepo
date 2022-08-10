011a938153114517cc9d5c291efb3498b3bf8462
[metehan mengen] <[valid-email]>
2022-08-10 14:31:31 +0300

    asal sayılar

---
+++ Sample/src/com/metehanmengen/app/exercises/FibonacciNumber.java
@@ -0,0 +1,24 @@
+package com.metehanmengen.app.exercises;
+
+public class FibonacciNumber {
+    public static void fibonacciNumber(){
+        //ilk 20 fib sayısını bulan döngü
+        for (int i = 1; i <= 20; i++)
+            System.out.print(getFibonacciNumber(i) +" ");
+    }
+    public static int getFibonacciNumber(int n)
+    {
+        if (n <= 2)
+            return n - 1;
+
+        int prev1 = 1, prev2 = 0, val = 0;
+
+        for (int i = 2; i < n; ++i) {
+            val = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = val;
+        }
+
+        return val;
+    }
+}
