5da9f8d966f692ad16ff3b545df1457908083c99
[metehan mengen] <[valid-email]>
2022-08-11 16:06:51 +0300

    MaxMin Values

---
+++ Sample/src/com/metehanmengen/app/exercises/arrays/BLetter.java
@@ -0,0 +1,29 @@
+package com.metehanmengen.app.exercises.arrays;
+
+public class BLetter {
+    public static void run()
+    {
+        String[][] letter = new String[7][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 3 || i== 6) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
